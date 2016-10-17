79686b2ed27e05d181460d5dd9bf36674bc3eea4
L-Dogg <szymek94sul@gmail.com>
2016-10-17 20:51:16 +0200

    Add point extender, add new project.

---
+++ GK1/States/IState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GK1.States
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public interface IState
+	{
+		public void MouseDown()
+	}
+}
