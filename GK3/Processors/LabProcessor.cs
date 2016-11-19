6133367a1f6fc98baf2be22698cf56cfb85f3972
L-Dogg <szymek94sul@gmail.com>
2016-11-19 20:45:13 +0100

    Add initial Lab Processor.

---
+++ GK3/Processors/LabProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GK3.Utilities;
+
+namespace GK3.Processors
+{
+	public class LabProcessor : IProcessor
+	{
+		public void Process(int a, int r, int g, int b, DirectBitmap bmp1, DirectBitmap bmp2, DirectBitmap bmp3, int x, int y)
+		{
+			
+		}
+	}
+}
