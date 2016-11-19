68d01f0d35a0763f3d3a53884f6dae2e9dc224a8
L-Dogg <szymek94sul@gmail.com>
2016-11-19 20:41:14 +0100

    Finish HSV Processor.

--- GK3/Processors/HSVProcessor.cs
+++ GK3/Processors/HSVProcessor.cs
@@ -17,8 +17,8 @@ namespace GK3.Processors
 
 			double H, S, V;
 
-			var Cmax = V = new double[] { r, g, b }.Max();
-			var Cmin = new double[] { r, g, b }.Min();
+			var Cmax = V = new [] { r, g, b }.Max();
+			var Cmin = new [] { r, g, b }.Min();
 			var delta = Cmax - Cmin;
 			
 			if (delta == 0.0) H = 0;
@@ -27,6 +27,12 @@ namespace GK3.Processors
 			else H = 60*((r - g)/delta + 4);
 
 			S = Cmax == 0.0 ? 0 : delta/Cmax;
+			var H_ = (int) (H/360*255);
+			var S_ = (int) (S*255);
+			var V_ = (int) (V*255);
+			bmp1[x, y] = a << 24 | (H_ << 16) | (H_ << 8) | H_;
+			bmp2[x, y] = a << 24 | (S_ << 16) | (S_ << 8) | S_;
+			bmp3[x, y] = a << 24 | (V_ << 16) | (V_ << 8) | V_;
 		}
 	}
 }
